1187fe72635180fdd9861f6ae5bcf703a1d3df1b
Hoonio <hoonio@hoonio.com>
2015-02-02 00:27:02 +0000

    testing hello world on Azure

--- server/app.js
+++ server/app.js
@@ -15,7 +15,10 @@ var app = express();
 
 var http = require('http')
 var port = process.env.PORT || 1337;
-var server = http.createServer(app);
+var server = http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port);
 
 require('./config/express')(app);
 require('./routes')(app);
